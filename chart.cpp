7ddb5ae01071b3c6e8b1762f6efc153e4ec3bd61
Alexander Elberling <thenameisbam@gmail.com>
2012-09-05 16:48:35 -0700

    input and largest work, display doesnt

--- chart.cpp
+++ chart.cpp
@@ -1,3 +1,7 @@
+//chart.cpp
+//elberling, Ace
+//elberling
+
 //Ace Elberling CSCI-211  chart.cpp 9/05/2012
 //this program creates a chart based on numbers input by the user
 
@@ -7,16 +11,40 @@ using namespace std;
 
 main()
 {
-  int numbers[100];
+  int num=1, numbers[100], large=0;
 
-  for(int i=0; numbers[i]<1; i++)
-    cin >> numbers[i];
+//input
+  for(int i=0; num>0; i++)
+    { 
+      cin>>num;  //takes numbers from user
+      numbers[i] = num;  //shoves number into array of numbers
+      if(large<numbers[i])//finds largest number input
+        large=numbers[i];
+    }
+cout<<"graph:\n";
+//display 
+  for(int row=0; large>row; row++)//row
+    {
+      for(int col=0; numbers[col]; col++)//colum
+        {
+        if(row==numbers[col])
+          cout<<"*";
+        else
+          cout<<" ";
+        }
+        cout<<endl;
+    }
+/*
+//display down, side left to right
+  for(int i=0; numbers[i]>0; i++)
+    {
+      for(int k=0; numbers[i]>k; k++)//outputs * based on number
+        cout<<"*";
+      cout<<endl;
+    }
   
-  cout<<"numbers recieved:\n";
-  for(int i=0; numbers[i]==0;i++)
-    cout<<numbers[i]<<" ";
-
   cout<<endl;
+*/
 
   return 0;
   
