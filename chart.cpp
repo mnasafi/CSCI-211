07fc90b381e3cb76998787c57c791cf2c3996376
Alexander Elberling <thenameisbam@gmail.com>
2012-09-05 18:28:43 -0700

    finished, takes numbers, finds largest, creates bar graph, plus extra graphs

--- chart.cpp
+++ chart.cpp
@@ -22,7 +22,7 @@ main()
         large=numbers[i];
     }
 
-//display 
+//display 1 
   for(int row=large; 0<row; row--)//row
     {
       for(int col=0; numbers[col]; col++)//colum
@@ -34,17 +34,50 @@ main()
         }
       cout<<endl;
     }
-/*
-//display down, side left to right
-  for(int i=0; numbers[i]>0; i++)
+
+  cout<<endl;
+//display 2 - 1 upside down
+  for(int row=0; large>row; row++)//row
     {
-      for(int k=0; numbers[i]>k; k++)//outputs * based on number
+      for(int col=0; numbers[col]; col++)//colum
+        {
+        if(row<numbers[col])
+          cout<<"*";
+        else
+          cout<<" ";
+        }
+      cout<<endl;
+    }
+
+  cout<<endl;
+
+//display 3, side left to right
+  for(int row=0; numbers[row]>0; row++)
+    {
+      for(int col=0; numbers[row]>col; col++)//outputs * based on number
         cout<<"*";
       cout<<endl;
     }
   
   cout<<endl;
-*/
+
+//display 4, side right to left
+  for(int row=0; numbers[row]>0; row++)
+    {
+      for(int col=large; 0<col; col--)
+        {
+        if(col<=numbers[row])
+          cout<<"*";
+        else
+          cout<<" ";
+        }
+      cout<<endl;
+    }
+  
+  cout<<endl;
+
+
+
   return 0;  
 }
 
