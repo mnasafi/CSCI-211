6b7bb70d5240723320daee1d2ac8bb9a02eb70b2
Alexander Elberling <thenameisbam@gmail.com>
2012-09-20 15:28:31 -0700

    replaced for with while getline still broke

--- main.cpp
+++ main.cpp
@@ -11,47 +11,30 @@ int main()
 {
   string sort_type, title, url, comment;
   Video* videos[100];
-  int rating;
+  int rating, i=0;
   float length;
   
 
   getline(cin, sort_type);
   cout<<"sort type: "<<sort_type<<endl;
-  for(int i=0; i<10; i++)
-    {
+
+//  for(int i=0; i<10; i++)
+//    {
       while(getline(cin, title));
-      while(getline(cin, url));
-      while(getline(cin, comment));
-      cin>>length>>rating;
-      cin.ignore();
+      {
+        getline(cin, url);
+        getline(cin, comment);
+        cin>>length>>rating;
+        cin.ignore();
       
       cout<<"input recieved\ntitle: "<<title<<"\nURL: "<<url<<endl;
 
       videos[i] = new Video(title, url, comment, length, rating);
-      
+      i++;
     }
 
   videos[0]->print();
 
-/*  
-  switch(sort_type)
-    {
-      case 'rating':
-        cout<<"sort rating\n";
-        break;
-      case 'length':
-        cout<<"sort length\n";
-        break;
-      case 'title':
-        cout<<"sort title\n;
-        break;
-      default:
-        cout<<"oops bugging out\n;
-        return 1;
-        //break;
-      
-    }
-*/  
 //  Video video1("Title One", "www.youtube.com/one", "Comment ONE", 1.1, 1);
 //  Video video2("Title Two", "www.youtube.com/two", "Comment TWO", 2.2, 2);
 
