f7cb6b05b5e3c9c0d4473c966b18ecd9fb6a18cb
Alexander Elberling <thenameisbam@gmail.com>
2012-10-15 18:43:22 -0700

    finished mycat, working on add

--- lab08/add/add.cpp
+++ lab08/add/add.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
 using namespace std;
+
+bool legal_int(char *str)
+{
+    for(int temp=0; str[temp]!=0;temp++)
+    {
+      if (isdigit(str[temp]))
+        return true;
+    }
+        return false;
+    
+}
 
 
 int main(int argc, char *argv[])
 {
-    cout << "replace this with code with code that adds are the arguments\n";
+  string buffer;
+  int i=1, value=0;
+
+  if (argc < 1)
+  {
+    cerr << "Error:illegal integer./n";
+    return 1;
+  }
+
+  while (argv[i])
+    {
+      if(legal_int(argv[i]))
+      {
+        value = atoi(argv[i]) + value;
+        i++;
+      }else
+         cerr<< "Error: illegal integer.\n";
+ 
+    }
+  
+  cout <<value<<endl;
+
+  return 0;
 }
--- lab08/mycat/mycat.cpp
+++ lab08/mycat/mycat.cpp
@@ -1,28 +1,7 @@
-/*
-    Simple program to demonstrate file I/O and 
-    how to access command line arguments.  For example:
+//mycat.cpp
+//ace elberling
+//elberling
 
-        $ mycat myinput 
-
-    "myinput" is specified on the command line when the program mycat is started.
-
-    This string (called a command line argument) is available in the main() 
-
-    This program is similar to the Linux cat program.  It accepts a single
-    command line argument (the name of a file), then it opens the file and 
-    reads each line from the file and writes that line to standard output
-
-        $ cat myfile
-        one
-        two
-        three
-        $ mycat myfile
-        one
-        two
-        three
-        $
-
-*/
 
 #include <iostream>
 #include <fstream>
@@ -38,13 +17,14 @@ int main(int argc, char *argv[])
     // argc tells us how many command line arguments were given
     // "1" means that no command line arguments were given
     // The first argument is the name of the executable
-    if (argc < 1)
+    if (argc < 2)
     {
         // write to standar error (cerr)
-        cerr << "No filename specified." << endl;
+        cerr << "Must specify input and output file." << endl;
         return 1; // 1 is error condition
     }
-    if (argc > 2)
+    
+    if (argc > 3)
     {
         cerr << "Too many command line arguments specified." << endl;
         return 1;
@@ -53,6 +33,8 @@ int main(int argc, char *argv[])
     // open the file specified in argv[1].  
     // The "ios::in" opens the file in read mode.
     ifstream my_ifile(argv[1], ios::in);
+    
+  ofstream  my_ofile(argv[2], ios::out);  // filename is a string holding the filename
 
     // my_ifile is an ifstream object connected with the given filename
     // if the contructor failed (could not open file) then !my_file returns true
@@ -63,6 +45,14 @@ int main(int argc, char *argv[])
         return 1; // error
     }
 
+    if (!my_ofile)
+    {
+        cerr << "Could not open output file <" << argv[2] << ">." << endl;
+        return 1; // error
+    }
+
+
+
     // buffer to hold input line
     string buffer;
 
@@ -70,7 +60,7 @@ int main(int argc, char *argv[])
     while (getline(my_ifile, buffer, '\n') != false)
     {
         // write the line to standard output
-        cout << buffer << endl;
+        my_ofile << buffer << endl;
     }
 
     return 0; // everything is ok
