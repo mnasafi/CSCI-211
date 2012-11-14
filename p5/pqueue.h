2dd7f72977912cdd19937074b2d5337c5838f468
Alexander Elberling <thenameisbam@gmail.com>
2012-11-13 16:03:28 -0800

    step one

--- p5/pqueue.h
+++ p5/pqueue.h
@@ -6,3 +6,29 @@
 #define _PQUEUE_H_
 
 #include<iostream>
+#include<string>
+#include "cust.h"
+
+using namespace std;
+
+class Pqueue
+{
+  private:
+    class Node
+    {
+      public:
+        Node(Cust *cust, Node *next)
+         {m_cust = cust;m_next = next;
+        Cust *m_cust;
+        Node *m_next;
+        ~Node(){delete m_cust;}
+    };
+    Mode *m_head;
+  public:
+    Pqueue():m_head(NULL){}
+    void enque(Cust *cust);
+    Cust *dequeue();
+  
+};
+
+
