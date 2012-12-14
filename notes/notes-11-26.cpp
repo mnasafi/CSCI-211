f0486bde1d04e07f8ff724904e20100f5c0678dc
Alexander Elberling <thenameisbam@gmail.com>
2012-12-14 13:08:05 -0800

    updated"

--- notes/notes-11-26.cpp
+++ notes/notes-11-26.cpp
@@ -51,4 +51,59 @@ delete node
 *****************/
 
 
+bool Tree::insert(int value, Node *&cur_root)
+{
+  if (cur_root == NULL)
+  {
+    cur_root = new Node(value);
+    return true;
+  }
+  else if (cur_root->m_value == value)
+    return false; //already in tree
+  else if (cur_root->m_value > value)
+    return insert(value, cur_root->m_left);
+  else
+    return insert(value, cur_root->m_right);
+}
 
+
+void Tree::depth(Node *root)
+{
+  if (root)
+  {
+    depth(root->m_left);
+    cout<<root->m_value<<endl;
+    depth(root->m_right);
+  }
+}
+
+
+//program efficiency
+////rule 1
+//    get it clear and correct before you worry about efficiency
+////rule 2
+//    you are prob wrong
+
+
+//order of magnitude
+//  
+
+//Final notes
+
+////any tree question, prob recursive
+////leaf is a node with no children
+int Tree::count_leaves() //public
+{
+ //base case, no children 
+  return count_leaves(m_root);
+}
+
+int Tree::count_leaves(Node *cur_root)//private
+{
+  if(!cur_root)
+    return 0;
+  if(!cur_root->m_left && !cur_root->m_right)
+    return 1;
+  return count_leaves(cur_root->m_left) + count_leaves(cur_root->m_right);
+  
+}
